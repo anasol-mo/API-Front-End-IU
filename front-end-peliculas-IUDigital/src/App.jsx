7ea13255eca6ad7a61b13bee1b7bf95f540e8711
anasol-mo <sol.murillo@est.iudigital.edu.co>
2025-04-06 21:29:31 -0500

    Revert "Actualizacion"
    This reverts commit 0932a00a5dcca32f67bf29de52e3e3fe4d685d23.

--- front-end-peliculas-IUDigital/src/App.jsx
+++ front-end-peliculas-IUDigital/src/App.jsx
@@ -1,28 +1,69 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import GeneroPage from './pages/GeneroPage';
-import DirectorPage from './pages/DirectorPage';
-import ProductoraPage from './pages/ProductoraPage';
-import TipoPage from './pages/TipoPage';
-import MediaPage from './pages/MediaPage';
 
-const App = () => {
+import './App.css'
+import Pelicula from './componentes/Pelicula'
+import NavBar from './componentes/NavBar';
+import {
+  Route,Routes
+} from "react-router-dom";
+import PeliculaDetalle from './componentes/PeliculaDetalle';
+import PeliculaEditar from './componentes/PeliculaEditar';
+import PeliculaNew from './componentes/PeliculaNew'
+import Genero from './componentes/Genero';
+import GeneroEdit from './componentes/GeneroEdit';
+import GeneroNew from './componentes/GeneroNew';
+import Director from './componentes/Director';
+import DirectorNew from './componentes/DirectorNew';
+import DirectorEdit from './componentes/DirectorEdit';
+import Productora from './componentes/Productora';
+import ProductoraEdit from './componentes/ProductoraEdit';
+import ProductoraNew from './componentes/ProductoraNew';
+import Tipo from './componentes/Tipo';
+import TipoNew from './componentes/TipoNew';
+import TipoEdit from './componentes/TipoEdit';
+
+
+
+
+
+
+function App() { 
+
+
+
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/genero" element={<GeneroPage />} />
-        <Route path="/director" element={<DirectorPage />} />
-        <Route path="/productora" element={<ProductoraPage />} />
-        <Route path="/tipo" element={<TipoPage />} />
-        <Route path="/media" element={<MediaPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+    <>
+      <header>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Pelicula/>} />
+          <Route path='/nueva-pelicula' element={<PeliculaNew/>} />
+          <Route path='/:id' element={ <PeliculaDetalle/>} />
+          <Route path='/editar/:id' element={<PeliculaEditar/>} />
+          <Route path='/generos' element={<Genero/>} />
+          <Route path='/generoEdit/:id' element={<GeneroEdit/>} />
+          <Route path='/generoNew' element={<GeneroNew/>} />
+          <Route path='/directores' element={<Director/>} />
+          <Route path='/directorEdit/:id' element={<DirectorEdit/>} />
+          <Route path='/directorNew' element={<DirectorNew/>} />
+          <Route path='/productoras' element={<Productora/>} />
+          <Route path='/productoraEdit/:id' element={<ProductoraEdit/>} />
+          <Route path='/productoraNew' element={<ProductoraNew/>} />
+          <Route path='/tipos' element={<Tipo/>} />
+          <Route path='/tipoEdit/:id' element={ <TipoEdit/> } />
+          <Route path='/tipoNew' element={ <TipoNew/> } />
 
-export default App;
+          
+        </Routes>
+      </header>
+      
 
 
+
+
+
+    </>
+  )
+}
+
+export default App
